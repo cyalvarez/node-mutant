4a2ba9a8f70b52a116492a8909e7993cb209c72e
Cindy Alvarez <ingeneo-msp@hotmail.com>
2021-04-24 18:20:52 -0500

    Added instructions on first get

--- index.js
+++ index.js
@@ -14,7 +14,34 @@ app.use(express.json())
 app.use('/dna', require('./routes/dna.routes'))
 
 app.get('/', (req, res) => {
-	res.send('<html><body><h1>Server Running &#x269B;</h1></body></html>')
+	res.send(`<html>
+	<body>
+	<h1>Server Running &#x269B;</h1>
+	<article>
+	<h2>REST API</h2>
+	<h3>Revisar ADN</h3>
+	<h4>Request</h4>
+	<p><code>POST /dna</code></p>
+	<h4>Body</h4>
+	<p>Ejemplo</p>
+	<pre><code>"dna": ["atcaga","cgaaac","accagc","aaacgc","ctgcac","acgtgc"]</code></pre>
+	<h4>Response</h4>
+	<pre><code>Status Code: 200 OK 
+"is a mutant"
+
+Status Code: 403 Forbidden
+"is not a mutant"
+</code></pre>
+	<h3>Ver estadisticas</h3>
+	<h4>Request</h4>
+	<p><code>GET /dna/stats</code></p>
+	<h4>Response</h4>
+	<pre><code>Status Code: 200 OK 
+{"count_mutant_dna":0,"count_human_dna":0,"ratio":0}
+</code></pre>
+	</article>
+	</body>
+	</html>`)
 })
 
 //Connect to DB
